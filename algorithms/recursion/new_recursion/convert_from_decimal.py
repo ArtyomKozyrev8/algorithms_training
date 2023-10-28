c5533e2c34bde37b75a7a653625d9146526f2fc9
ArtyomKozyrev8 <kozirev8@gmail.com>
2023-10-28 16:12:44 +0300

    remastered convert_from_decimal.py

--- algorithms/recursion/new_recursion/convert_from_decimal.py
+++ algorithms/recursion/new_recursion/convert_from_decimal.py
@@ -1,22 +1,42 @@
-def convert_from_dec(number: int, base: int) -> str:
-    """
-    Coverts number from decimal to bin, oct or hex system
-    :param number: some positive decimal number
-    :param base: 2, 8, 16
-    :return: string representation of number in the desired format
-    """
-    base_digits = "0123456789ABCDEF"
-    if number < base:
-        return base_digits[number]
+from enum import Enum
 
-    return convert_from_dec(number // base, base) + base_digits[number % base]
+
+class NumericSystemEnum(Enum):
+    binary = 2
+    oct = 8
+    decimal = 10
+
+
+def convert_from_decimal_to_another(number: int, num_system: NumericSystemEnum) -> int:
+    base = num_system.value
+    result_string = ""
+    if number < base:
+        result_string = str(number) + result_string
+    else:
+        result_string = convert_from_decimal_to_another(number // base, num_system) + str(number % base)
+
+    return result_string
 
 
 if __name__ == '__main__':
-    for i in range(0, 20):
-        print(f"{i} : {convert_from_dec(i, 2)}")
-    for i in range(0, 20):
-        print(f"{i} : {convert_from_dec(i, 16)}")
-    for i in range(224, 256):
-        print(f"{i} : {convert_from_dec(i, 16)}")
-    print(f"{255} : {convert_from_dec(i, 8)}")
+    inputs_ = (
+        (11, NumericSystemEnum.binary, "1011"),
+        (0, NumericSystemEnum.binary, "0"),
+        (8, NumericSystemEnum.binary, "1000"),
+        (2, NumericSystemEnum.binary, "10"),
+        (1, NumericSystemEnum.binary, "1"),
+        (13, NumericSystemEnum.decimal, "13"),
+        (0, NumericSystemEnum.decimal, "0"),
+        (8, NumericSystemEnum.decimal, "8"),
+        (2, NumericSystemEnum.decimal, "2"),
+        (1, NumericSystemEnum.decimal, "1"),
+        (28, NumericSystemEnum.oct, "34"),
+        (344, NumericSystemEnum.oct, "530"),
+        (0, NumericSystemEnum.oct, "0"),
+        (8, NumericSystemEnum.oct, "10"),
+        (2, NumericSystemEnum.oct, "2"),
+        (1, NumericSystemEnum.oct, "1"),
+    )
+
+    for item in inputs_:
+        assert convert_from_decimal_to_another(item[0], item[1]) == item[2]
