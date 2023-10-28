d6129aa5adf7e260ff4fbb062341bff70ddd7f9d
ArtyomKozyrev8 <kozirev8@gmail.com>
2023-10-28 21:14:55 +0300

    greedy_method.py

---
+++ algorithms/exchange_coins_solutions/greedy_method.py
@@ -0,0 +1,40 @@
+from typing import Optional, List, Tuple
+
+
+COINS = [1, 5, 10, 50, 100]
+
+
+def exchange_greedy(
+        number: int,
+        coins: Optional[List[int]] = None,
+) -> Tuple[int, List[int]]:
+    """Greedy method to exchange a certain number."""
+    used_coins = []
+    if coins is None:
+        coins = COINS
+
+    coins = sorted(coins, reverse=True)
+    coins_num = 0
+
+    for coin in coins:
+        while number >= coin:
+            coins_num += 1
+            number -= coin
+            used_coins.append(coin)
+
+    return coins_num, sorted(used_coins)
+
+
+if __name__ == '__main__':
+    # structure of inputs items: (number, coins_num_to_exchange_the_sum, coins_used)
+    inputs = (
+        (101, 2, [1, 100]),
+        (0, 0, []),
+        (99, 10, [1, 1, 1, 1, 5, 10, 10, 10, 10, 50]),
+        (1, 1, [1]),
+        (2, 2, [1, 1]),
+        (7, 3, [1, 1, 5]),
+    )
+
+    for item in inputs:
+        exchange_greedy(item[0]) == item[1], item[2]
