bf7c96c4e86faba0dff0f329d1c644c7601a66a1
ArtyomKozyrev8 <kozirev8@gmail.com>
2023-10-28 15:30:21 +0300

    remastered is_palindrome.py

--- algorithms/recursion/new_recursion/is_palindrome.py
+++ algorithms/recursion/new_recursion/is_palindrome.py
@@ -1,16 +1,36 @@
-def is_palindrome(x: str) -> str:
-    """Checks if the string is palindrome"""
-    if len(x) <= 1:
+import string
+
+
+def keep_only_letters(string_: str) -> str:
+    new_str = []
+    for letter in string_.lower():
+        if letter in string.ascii_lowercase:
+            new_str.append(letter)
+
+    return "".join(new_str)
+
+
+def is_palindrome(string_: str) -> bool:
+    if len(string_) <= 1:
         return True
-    if x[0] != x[-1]:
+    elif string_[-1] != string_[0]:
         return False
-    return is_palindrome(x[1:len(x) - 1])
+    else:
+        return is_palindrome(string_[1: len(string_) - 1])
 
 
 if __name__ == '__main__':
-    print(is_palindrome("abcd"))
-    print(is_palindrome(""))
-    print(is_palindrome("a"))
-    print(is_palindrome("aa"))
-    print(is_palindrome("ab"))
-    print(is_palindrome("aba"))
+    # structure of strings items: (string, is_palindrome answer)
+    strings = (
+        ("", True),
+        ("x", True),
+        ("radar", True),
+        ("hello", False),
+        ("hannah", True),
+        ("madam i'm adam", True),
+        ("Reviled did I live, said I, as evil I did deliver", True),
+        ("Go hang a salami; I’m a lasagna hog.", True),
+    )
+
+    for item in strings:
+        assert is_palindrome(keep_only_letters(item[0])) is item[1]
