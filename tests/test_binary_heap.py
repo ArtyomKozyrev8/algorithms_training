6100a01bd969c5f189dbf3beb91c81cb89aa9088
ArtyomKozyrev8 <kozirev8@gmail.com>
2023-11-14 20:21:05 +0300

    add delete method to BinaryHeap structure

--- tests/test_binary_heap.py
+++ tests/test_binary_heap.py
@@ -1,4 +1,3 @@
-import py
 import pytest
 
 from trees import BinaryHeap
@@ -26,3 +25,34 @@ def test_insert(input_) -> None:
         s.insert(i)
 
     assert str(s) == f"BinaryHeap({input_})"
+
+
+def test_delete() -> None:
+    s = BinaryHeap()
+    for i in [5, 9, 11, 14, 18, 19, 21, 33, 17, 27]:
+        s.insert(i)
+
+    assert str(s) == "BinaryHeap([5, 9, 11, 14, 18, 19, 21, 33, 17, 27])"
+
+    s.delete() == 5
+    assert str(s) == "BinaryHeap([9, 14, 11, 17, 18, 19, 21, 33, 27])"
+    s.delete() == 9
+    assert str(s) == "BinaryHeap([11, 14, 19, 17, 18, 27, 21, 33])"
+    s.delete() == 11
+    assert str(s) == "BinaryHeap([14, 17, 19, 33, 18, 27, 21])"
+    s.delete() == 14
+    assert str(s) == "BinaryHeap([17, 18, 19, 33, 21, 27])"
+    s.delete() == 17
+    assert str(s) == "BinaryHeap([18, 21, 19, 33, 27])"
+    s.delete() == 18
+    assert str(s) == "BinaryHeap([19, 21, 27, 33])"
+    s.delete() == 19
+    assert str(s) == "BinaryHeap([21, 33, 27])"
+    s.delete() == 21
+    assert str(s) == "BinaryHeap([27, 33])"
+    s.delete() == 27
+    assert str(s) == "BinaryHeap([33])"
+    s.delete() == 33
+    assert str(s) == "BinaryHeap([])"
+    with pytest.raises(IndexError):
+        s.delete()
--- trees/binary_heap.py
+++ trees/binary_heap.py
@@ -2,7 +2,7 @@ class BinaryHeap:
     """
     self._array = [] is the presentation of the structure, but the array can be shown as a binary tree.
 
-    The idea of the structure is is that root node is always minimum value.
+    The idea of the structure is that root node is always minimum value.
 
     Moreover, each subtree top value is the minimum value.
     """
@@ -11,6 +11,24 @@ class BinaryHeap:
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self._array})"
+
+    def __len__(self) -> int:
+        return len(self._array)
+
+    def get_min(self) -> int:
+        if self._array:
+            return self._array[0]
+
+        raise IndexError(f"{self.__class__.__name__} instance is empty!")
+
+    def is_empty(self) -> bool:
+        if self._array:
+            return True
+
+        return False
+
+    def size(self) -> int:
+        return len(self._array)
 
     def insert(self, value: int) -> None:
         self._array.append(value)  # append to end of the array
@@ -32,3 +50,62 @@ class BinaryHeap:
                 current_position = parent_position
             else:
                 break  # stop here!
+
+    def delete(self) -> int:
+        if self._array:
+            min_val = self._array[0]
+            if len(self._array) == 1:
+                self._array.pop()
+            else:
+                self._array[0], self._array[-1] = self._array[-1], self._array[0]  # change places before deleting
+                # then we start to move the item to bottom
+                self._array.pop()  # now remove minimum value from structure
+                # we should to remove "big" element from tail back
+                # (probably it is max element or close to max)
+                self._percolate_down(0)
+
+            return min_val
+
+        raise IndexError(f"{self.__class__.__name__} instance is empty!")
+
+    def _get_min_child_index(self, index: int) -> int:
+        max_index = len(self._array) - 1
+
+        left_child_index = 2 * index + 1  # probably out of range
+        right_child_index = 2 * index + 2  # probably out of range
+
+        if left_child_index > max_index:
+            raise IndexError(
+                f"{self.__class__.__name__} instance does not contain item with index {left_child_index}!"
+            )
+
+        if right_child_index > max_index:
+            min_child_index = left_child_index  # means only one child exists
+        else:
+            left_child = self._array[left_child_index]
+            right_child = self._array[right_child_index]
+
+            if left_child <= right_child:
+                min_child_index = left_child_index
+            else:
+                min_child_index = right_child_index
+
+        return min_child_index
+
+    def _percolate_down(self, start_position: int) -> None:
+        current_position = start_position
+
+        # 2 * current_position + 1 this is potential left child position
+        # we check if current node have any child
+        while 2 * current_position + 1 <= len(self._array) - 1:
+            min_child_index = self._get_min_child_index(current_position)
+            min_child_value = self._array[min_child_index]
+            current_value = self._array[current_position]
+            if current_value < min_child_value:
+                break
+            else:
+                self._array[min_child_index] = current_value
+                self._array[current_position] = min_child_value
+                current_position = min_child_index
+
+        return
